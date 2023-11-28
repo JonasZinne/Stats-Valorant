d4da8a538283c71ed1f3f1439544bdab3727af5c
JonasZinne <102172816+JonasZinne@users.noreply.github.com>
2023-11-28 16:12:08 +0100

    Version 1.21

--- styles.py
+++ styles.py
@@ -3,25 +3,61 @@ def set_window_styles(window):
     window.configure(bg="#483D8B")
 
 def set_label_styles(label):
-    label.config(font=("Arial", 14, "bold"), pady=5, bg="#483D8B")
+    label.config(
+        font=("Arial", 14, "bold", "underline"), 
+        bg="#483D8B", 
+        fg="#ff8c00"
+    )
 
 def set_entry_styles(entry):
-    entry.config(font=("Arial", 12))
+    entry.config(
+        font=("Arial", 12), 
+        width=20
+    )
 
 def set_button_styles(button):
-    button.config(font=("Arial", 12))
+    button.config(
+        font=("Arial", 12),
+        width=13,
+        height=1,
+        borderwidth=2,
+        relief="solid",
+        cursor="hand2"
+    )
 
 def set_title_label_styles(label):
-    label.config(font=("Arial", 24, "bold"), pady=20, bg="#483D8B")
+    label.config(
+        font=("Arial", 24, "bold", "underline"), 
+        bg="#483D8B"
+    )
 
 def set_data_label_styles(label):
-    label.config(font=("Arial", 14), padx=20, pady=10, bg="#483D8B")
+    label.config(
+        font=("Arial", 14), 
+        bg="#483D8B",
+        padx=20, 
+        pady=10
+    )
 
 def set_error_label_styles(label):
-    label.config(font=("Arial", 18, "bold"), padx=20, pady=10, bg="#483D8B", fg="red")
+    label.config(
+        font=("Arial", 18, "bold"), 
+        bg="#483D8B", 
+        fg="red",
+        padx=20,
+        pady=10
+    )
 
 def set_credits_label_styles(label):
-    label.config(font=("Arial", 9), bg="#483D8B")
+    label.config(
+        font=("Arial", 9), 
+        bg="#483D8B"
+    )
 
 def set_version_label_styles(label):
-    label.config(font=("Arial", 8), pady=5, bg="#483D8B")
+    label.config(
+        font=("Arial", 8), 
+        bg="#483D8B", 
+        fg="grey",
+        pady=5
+    )
